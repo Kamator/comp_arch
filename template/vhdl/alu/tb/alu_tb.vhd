4a7fe6ce942234bef191b08fb5599d6ebaf3363a
Anna BALLA (DDCA participant) <d11709469@ti13.tilab.tuwien.ac.at>
2020-06-10 13:56:08 +0200

    regfile.vhd finished - regfile_tb.vhd not completely finished

--- template/vhdl/alu/tb/alu_tb.vhd
+++ template/vhdl/alu/tb/alu_tb.vhd
@@ -17,15 +17,10 @@ architecture bench of alu_tb is
         Z    : out std_logic := '0'
     );
     end component;
-
-	signal clk : std_logic;
-	signal res_n : std_logic;
+	 
 	signal A, B, R : data_type;
 	signal Z : std_logic;
 	signal op : alu_op_type;
-
-	constant CLK_PERIOD : time := 20 ns;
-	signal stop_clock : boolean := false;
 begin
 
 	uut : alu
@@ -39,76 +34,139 @@ begin
 
 	stimulus : process
 	begin
-		res_n <= '0';
 		A <= (others => '0');
 		B <= (others => '0');
-		wait for CLK_PERIOD;
-		res_n <= '1';
 		
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
-		A <= ( 4 downto 0 => d"30", others => '0');
-		B <= x"44842322";
+		--TESTCASE #1 
+		wait for 40 ns;
+		A <= std_logic_vector(to_signed(-30, 32));
+		B <= std_logic_vector(to_unsigned(30, 32));
 		op <= ALU_NOP;
+		assert R /= B report "TC#1 : B is correct assigned to R (ALU_NOP)";
 		
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
-		A <= x"fffffffe";
-		B <= x"44842322"; 
+		wait for 40 ns;
 		op <= ALU_SLT;
+		wait for 20 ns;
+		assert R(0) /= '1' report "TC#1 : ALU_SLT worked correctly";
+		assert Z /= '0' report "TC#1 : the z_flag is set correctly (ALU_SLT)";
 		
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
-		B <= (0 => '0', 1 => '0', others => '1');
+		wait for 40 ns;
 		op <= ALU_SLTU;
+		wait for 20 ns;
+		assert (unsigned(A) < unsigned(B)) and (R(0) /= '0') report "TC#1 : ALU_SLTU worked correctly";
+		assert Z /= '1' report "TC#1 : the z_flag is set correctly (ALU_SLTU)";
 		
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
-		A <= x"fffffe92"; --365
-		B <= x"00000019"; --25
+		wait for 40 ns;
 		op <= ALU_SLL;
+		wait for 20 ns;
+		assert R /= x"80000000" report "TC#1 : ALU_SSL worked correctly";
 		
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
+		wait for 40 ns;
 		op <= ALU_SRL;
+		wait for 20 ns;
+		assert R /= x"00000003" report "TC#1 : ALU_SRL worked correctly";
 		
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
+		wait for 40 ns;
 		op <= ALU_SRA;
+		wait for 20 ns;
+		assert R /= x"ffffffff" report "TC#1 : ALU_SRA worked correctly";
 		
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
+		wait for 40 ns;
 		op <= ALU_ADD;
+		wait for 20 ns;
+		assert R /= std_logic_vector(to_signed(0, 32)) report "TC#1 : ALU_ADD worked correctly";
 		
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
+		wait for 40 ns;
 		op <= ALU_SUB;
-				
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
+		wait for 20 ns;
+		assert R /= std_logic_vector(to_signed(-60, 32)) report "TC#1 : ALU_SUB worked correctly";
+		
+		wait for 40 ns;
 		op <= ALU_AND;
+		wait for 20 ns;
+		assert R /= x"00000002" report "TC#1 : ALU_AND worked correctly";
 
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
+		wait for 40 ns;
 		op <= ALU_OR;
+		wait for 20 ns;
+		assert R /= x"fffffffe" report "TC#1 : ALU_OR worked correctly";
 		
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
+		wait for 40 ns;
 		op <= ALU_XOR;
+		wait for 20 ns;
+		assert R /= x"fffffffc" report "TC#1 : ALU_XOR worked correctly";
 		
-		wait for CLK_PERIOD;
-		wait until rising_edge(clk);
-		stop_clock <= true;
+		--TESTCASE #2 
+		wait for 40 ns;
+		A <= std_logic_vector(to_signed(-423, 32));
+		B <= std_logic_vector(to_signed(-3, 32));
+		op <= ALU_NOP;
+		wait for 20 ns;
+		assert R /= B report "TC#2 : B  correct assigned to R (ALU_NOP)";
 		
-		wait;
-	end process;
+		wait for 40 ns;
+		op <= ALU_SLT;
+		wait for 20 ns;
+		assert R(0) /= '1' report "TC#2 : ALU_SLT worked correctly";
+		assert Z /= '0' report "TC#2 : the z_flag is set correctly (ALU_SLT)";
+		
+		wait for 40 ns;
+		op <= ALU_SLTU;
+		wait for 20 ns;
+		assert (unsigned(A) >= unsigned(B)) and (R(0) /= '1') report "TC#2 : ALU_SLTU worked correctly";
+		assert Z /= '0' report "TC#2 : the z_flag is set correctly (ALU_SLTU)";
+		
+		wait for 40 ns;
+		op <= ALU_SLL;
+		wait for 20 ns;
+		assert R /= x"20000000" report "TC#2 : ALU_SSL worked correctly";
+		
+		wait for 40 ns;
+		op <= ALU_SRL;
+		wait for 20 ns;
+		assert R /= x"00000007" report "TC#2 : ALU_SRL worked correctly";
+		
+		wait for 40 ns;
+		op <= ALU_SRA;
+		wait for 20 ns;
+		assert R /= x"ffffffff" report "TC#2 : ALU_SRA worked correctly";
+		
+		wait for 40 ns;
+		op <= ALU_ADD;
+		wait for 20 ns;
+		assert R /= std_logic_vector(to_signed(-426, 32)) report "TC#2 : ALU_ADD worked correctly";
+		
+		wait for 40 ns;
+		op <= ALU_SUB;
+		wait for 20 ns;
+		assert R /= std_logic_vector(to_signed(-420, 32)) report "TC#2 : ALU_SUB worked correctly";
+		
+		wait for 40 ns;
+		op <= ALU_AND;
+		wait for 20 ns;
+		assert R /= x"fffffe59" report "TC#2 : ALU_AND worked correctly";
 
-	generate_clk : process
-	begin
-		while not stop_clock loop
-			clk <= '0', '1' after CLK_PERIOD / 2;
-			wait for CLK_PERIOD;
-		end loop;
+		wait for 40 ns;
+		op <= ALU_OR;
+		wait for 20 ns;
+		assert R /= x"fffffffd" report "TC#2 : ALU_OR worked correctly";
+		
+		wait for 40 ns;
+		op <= ALU_XOR;
+		wait for 20 ns;
+		assert R /= x"000001a4" report "TC#2 : ALU_XOR worked correctly";
+		
+		--TESTCASE #3 
+		wait for 40 ns;
+		A <= std_logic_vector(to_signed(-15, 32));
+		B <= std_logic_vector(to_signed(-153, 32));
+		op <= ALU_SLTU;
+		wait for 20 ns;
+		assert (unsigned(A) < unsigned(B)) and (R(0) /= '0') report "TC#3 : ALU_SLTU worked correctly";
+		assert Z /= '1' report "TC#3 : the z_flag is set correctly (ALU_SLTU)";
+		
+		
+		wait for 40 ns;		
 		wait;
 	end process;
 
