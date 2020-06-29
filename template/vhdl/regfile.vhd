08fb51fd3050754105a12de54ba7fe27246351f6
Anna BALLA (DDCA participant) <d11709469@ti13.tilab.tuwien.ac.at>
2020-06-29 14:00:38 +0200

    update: regfile.vhd - removed two metavalue warnings & fixed delay; edited wave.do (regfile tb); added 2 signals to wave.do (pipeline tb)

--- template/vhdl/regfile.vhd
+++ template/vhdl/regfile.vhd
@@ -21,35 +21,47 @@ end entity;
 architecture rtl of regfile is
     type REG_MEMORY is array (0 to REG_COUNT-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
     signal reg_file : REG_MEMORY := (others => (others => '0'));
-    signal tmp_rdaddr1, tmp_rdaddr2 : reg_adr_type := (others => '0');
+    signal int_rdaddr1, int_rdaddr2 : reg_adr_type := (others => '0');
+	 signal latch_rdaddr1, latch_rdaddr2 : reg_adr_type := (others => '0');
+	 signal int_rddata1, int_rddata2 : data_type := (others => '0');
 	 constant zeros : std_logic_vector(reg_adr_type'range) := (others => '0');
+	 constant x_addr : std_logic_vector(reg_adr_type'range) := (others => 'X');
 begin
     sync : process(reset, clk, stall)
     begin
         if reset = '0' then
             reg_file <= (others => (others => '0'));
-            tmp_rdaddr1 <= (others => '0');
-            tmp_rdaddr2 <= (others => '0');
+            latch_rdaddr1 <= (others => '0');
+				latch_rdaddr2 <= (others => '0');
         elsif rising_edge(clk) and stall = '0' then --stall causes the circuit not to latch input values
-            tmp_rdaddr1 <= rdaddr1;
-            tmp_rdaddr2 <= rdaddr2;
+            latch_rdaddr1 <= int_rdaddr1;
+            latch_rdaddr2 <= int_rdaddr2;
 				if regwrite = '1' and wraddr /= zeros then
 					reg_file(to_integer(unsigned(wraddr))) <= wrdata;
+				else
+					reg_file(to_integer(unsigned(zeros))) <= (others => '0');
 				end if;    
         end if;
     
     end process;
+	 
+	 int_rdaddr1 <= rdaddr1;
+	 int_rdaddr2 <= rdaddr2;
+	 rddata1 <= int_rddata1;
+	 rddata2 <= int_rddata2;
     
     reg_file_read : process(all)
     begin
-        rddata1 <= reg_file(to_integer(unsigned(tmp_rdaddr1)));
-        rddata2 <= reg_file(to_integer(unsigned(tmp_rdaddr2)));
-        
-        if tmp_rdaddr1 = wraddr and tmp_rdaddr1 /= zeros and regwrite = '1' and stall = '0' then
-            rddata1 <= wrdata;
-        elsif tmp_rdaddr2 = wraddr and tmp_rdaddr2 /= zeros and regwrite = '1' and stall = '0' then
-            rddata2 <= wrdata;
-        end if;    
+		if stall = '0' and int_rdaddr1 /= x_addr and int_rdaddr2 /= x_addr and reset = '1' then
+			int_rddata1 <= reg_file(to_integer(unsigned(int_rdaddr1)));
+			int_rddata2 <= reg_file(to_integer(unsigned(int_rdaddr2)));
+		  
+			if int_rdaddr1 = wraddr and int_rdaddr1 /= zeros and regwrite = '1' then
+				int_rddata1 <= wrdata;
+			elsif int_rdaddr2 = wraddr and int_rdaddr2 /= zeros and regwrite = '1' then
+            int_rddata2 <= wrdata;
+			end if;
+		end if;		
     end process;
     
 end architecture;
